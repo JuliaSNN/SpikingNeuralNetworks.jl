a149a8e0725edeb6c102b18062a7feba570a01bd
Russell Jarvis <rjjarvis@asu.edu>
2020-03-21 16:17:08 +1100

    contents

---
+++ examples/lhhneuron.jl
@@ -0,0 +1,20 @@
+using Distributed
+addprocs(4)
+@everywhere include("local_hh_neuron.jl")
+using NSGAIII
+@everywhere include("../src/units.jl")
+@everywhere using .HHNSGA
+pop = pmap(HHNSGA.init_function,1:3)
+println(pop)
+
+scores = pmap(HHNSGA.z,pop)
+#=
+NSGAIII.indiv(copy(pop[1]),HHNSGA.z)
+
+println(scores)
+non_dom = NSGAIII.fast_non_dominated_sort!(scores)[1]
+pop = setdiff(pop, non_dom)
+HHNSGA.plot_pop(scores)
+=#
+@show(genes)
+@show(scores)
--- examples/local_hh_neuron.jl
+++ examples/local_hh_neuron.jl
@@ -1,240 +1,232 @@
-try
-    using Plots
-    using UnicodePlots
-    using OrderedCollections
-catch
-    import Pkg; Pkg.add("Plots")
-    Pkg.add("OrderedCollections") 
-    import Pkg; Pkg.add("UnicodePlots")
-    using OrderedCollections
-end
-include("../src/SpikingNeuralNetworks.jl")
-include("../src/units.jl")
-include("../src/plot.jl")
-try
-    using UnicodePlots
-    using PyCall
-    using NSGAIII
-catch
+
+
+module HHNSGA
     using Pkg
-    Pkg.add("PyCall")
+    # ENV["PYTHON"]="/home/user/anaconda3/bin/python"
+    # Pkg.build("PyCall")
     Pkg.add("UnicodePlots")
-    #Pkg.add("Conda")
-    #using Conda
-    #Conda.add("matplotlib")
-    Pkg.build("PyCall")
-    Pkg.clone("https://github.com/gsoleilhac/NSGAIII.jl")
-    using PyCall
-    using UnicodePlots
-end
-SNN = SpikingNeuralNetworks.SNN
+    #using Distributed
+    try
+        using Plots
+        using UnicodePlots
+        using OrderedCollections
+    catch
+        import Pkg; Pkg.add("Plots")
+        Pkg.add("OrderedCollections")
+        import Pkg; Pkg.add("UnicodePlots")
+        using OrderedCollections
 
-py"""
-import copy
+    end
+    include("../src/SpikingNeuralNetworks.jl")
+    import Pkg; Pkg.add("ProgressMeter")
+    #Pkg.add(PackageSpec(path="/opt/julia/dev/NSGAIII.jl"))
+    #Pkg.add()
+    using NSGAIII
+    export NSGAIII
+    #include("/opt/julia/dev/NSGAIII.jl/src/NSGAIII.jl")
+    include("../src/units.jl")
+    include("../src/plot.jl")
+    export nS
+    using Pkg
+    Pkg.resolve()
 
-import matplotlib
-import matplotlib.pyplot as plt
-import numpy as np
-matplotlib.get_backend()
-import sys
-import os
-sys.path.append(os.getcwd())
-"""
-py"""
-from neuronunit import tests
-from neuronunit.tests import fi
-from simple_with_injection import SimpleModel
-from neo import AnalogSignal
-import quantities as pq
-try:
-   from julia import Main
-except:
-   import julia
-   julia.install()
-   from julia import Main
+    try
+        using LinearAlgebra
+        using UnicodePlots
+        using PyCall
+        using NSGAIII
+    catch
+        using Pkg
+        Pkg.add("PyCall")
+        Pkg.add("UnicodePlots")
 
+        Pkg.build("PyCall")
+        Pkg.clone("https://github.com/gsoleilhac/NSGAIII.jl")
+        using PyCall
+        using UnicodePlots
+    end
+    export SNN
+    SNN = SpikingNeuralNetworks.SNN
 
-"""
-simple = py"SimpleModel()"
+    using Random
+    py"""
+    import copy
+    from neuronunit.optimisation import algorithms
+    from neuronunit.optimisation import optimisations
 
-py"""
-import pickle
-from neuronunit.tests.fi import RheobaseTestP
-
-cell_tests = pickle.load(open('multicellular_constraints.p','rb'))
-for test in cell_tests.values():
-    if "Rheobase test" in test.keys():
-        temp_test = {k:v for k,v in test.items()}
-        break
-rt = temp_test["Rheobase test"]
-
-rtest = RheobaseTestP(observation=rt.observation)
-#                        name='RheobaseTest')
-JHH = {
-'Vr': -68.9346,
-'Cm': 0.0002,
-'gl': 1.0 * 1e-5,
-'El': -65.0,
-'EK': -90.0,
-'ENa': 50.0,
-'gNa': 0.02,
-'gK': 0.006,
-'Vt': -63.0
-}
-JHH1 = {k:(v-0.01*v) for k,v in JHH.items()}
-JHH2 = {k:(v+0.01*v) for k,v in JHH.items()}
-ranges = {k:[v-0.01*v,v+0.01*v] for k,v in copy.copy(JHH).items()}
-
-"""
-#
-ranges = py"ranges"
-H1=[values(ranges)]
-
-current_params = py"rt.params"
-print(current_params)
-simple.attrs = py"JHH"
-
-#vm = simple.inject_square_current(current_params)
-print("gets here")
-print(typeof(simple))
-py"""
-model = SimpleModel()
-model2 = SimpleModel()
-model3 = SimpleModel()
-model.attrs = JHH
-model2.attrs = JHH1
-model3.attrs = JHH2
-
-import pickle
-rheo = rt.generate_prediction(model)
-print(rheo)
-rheo1 = rt.generate_prediction(model2)
-print(rheo1)
-rheo2 = rt.generate_prediction(model3)
-print(rheo2)
-
-"""
-py"""
-print("parallel Rheobase is currently broken for python as well need to fix in unit tests")
-
-#rheo_broken = rtest.generate_prediction(model)
-#print(rheo_broken)
-"""
-py"""
+    import matplotlib
+    import matplotlib.pyplot as plt
+    import numpy as np
+    matplotlib.get_backend()
+    import sys
+    import os
+    sys.path.append(os.getcwd())
+    """
+    py"""
+    from neuronunit import tests
+    from neuronunit.tests import fi
+    from simple_with_injection import SimpleModel
+    from neo import AnalogSignal
+    import quantities as pq
+    try:
+       from julia import Main
+    except:
+       import julia
+       julia.install()
+       from julia import Main
 
 
-def evaluate(tests,attrs):
-    model = SimpleModel()
-    model.attrs = attrs
+    """
+    simple = py"SimpleModel()"
 
-    from sciunit.scores.collections import ScoreArray
-    from sciunit import TestSuite
-    import sciunit
-    #from neuronunit.optimisation.data_transport_container import DataTC
-    scores_ = []
+    py"""
+    import pickle
+    import numpy as np
+    from neuronunit.tests.fi import RheobaseTestP
     from collections import OrderedDict
-    tests = OrderedDict({k:v for k,v in tests.items()})
-    for key, temp_test in tests.items():
-        if key == 'Rheobase test':
-            temp_test.score_type = sciunit.scores.ZScore
-        if key in ['Injected current AP width test', 'Injected current AP threshold test', 'Injected current AP amplitude test', 'Rheobase test']:
+    cell_tests = pickle.load(open('multicellular_constraints.p','rb'))
+    for test in cell_tests.values():
+        if "Rheobase test" in test.keys():
+            temp_test = {k:v for k,v in test.items()}
+            break
+    rt = temp_test["Rheobase test"]
+
+    rtest = RheobaseTestP(observation=rt.observation)
+    #                        name='RheobaseTest')
+    JHH = {
+    'Vr': -68.9346,
+    'Cm': 0.0002,
+    'gl': 1.0 * 1e-5,
+    'El': -65.0,
+    'EK': -90.0,
+    'ENa': 50.0,
+    'gNa': 0.02,
+    'gK': 0.006,
+    'Vt': -63.0
+    }
+    #JHH1 = {k:(v-0.01*v) for k,v in JHH.items()}
+    #JHH2 = {k:(v+0.01*v) for k,v in JHH.items()}
+    ranges = OrderedDict({k:[v-0.01*np.abs(v),v+0.01*np.abs(v)] for k,v in copy.copy(JHH).items()})
+    N = len(JHH)
+    model = SimpleModel()
+    """
+    N = py"N"
+
+    #
+    ranges = py"ranges"
+    H1=[values(ranges)]
+
+    current_params = py"rt.params"
+    print(current_params)
+    simple.attrs = py"JHH"
+
+    py"""
+    from neuronunit.optimisation import exhaustive_search
+    from sklearn.model_selection import ParameterGrid
+
+    grid = ParameterGrid(ranges)
+    genes = []
+    for g in grid: genes.append(g.values)
+    #dic_grid = es.create_grid(mp_in =ranges,npoints = 10, free_params =ranges.keys())
+
+    """
+    py"""
+    def evaluate(test,god):
+        model = SimpleModel()
+        model.attrs = god
+        rt = test['Rheobase test']
+
+        rheo = rt.generate_prediction(model)
+        from sciunit.scores.collections import ScoreArray
+        from sciunit import TestSuite
+        import sciunit
+        scores_ = []
+
+        for key, temp_test in test.items():
+            if key == 'Rheobase test':
+                temp_test.score_type = sciunit.scores.ZScore
+            if key in ['Injected current AP width test', 'Injected current AP threshold test', 'Injected current AP amplitude test', 'Rheobase test']:
+                rt.params['amplitude'] = rheo['value']
+
             try:
-                temp_test.params['injected_square_current']['amplitude'] = rheo['value']
+                score = temp_test.judge(model)
+                score = np.abs(score.log_norm_score)
+
             except:
-                pass
-        try:
-            score = temp_test.judge(model)
-        except:
-            score = -np.inf
-        if isinstance(score, sciunit.scores.incomplete.InsufficientDataScore):
-            score = -np.inf
-        else:
-            score = score
-        scores_.append(score)
-    tt = TestSuite(list(tests.values()))
-    SA = ScoreArray(tt, scores_)
-    return SA
+                score = 100#np.inf
+            if isinstance(score, sciunit.scores.incomplete.InsufficientDataScore):
+                score = 100#np.inf
+            scores_.append(score)
+        tt = TestSuite(list(test.values()))
+        SA = ScoreArray(tt, scores_)
+        errors = tuple(SA.values,)
+        return errors
+    """
 
-cell_tests = pickle.load(open('multicellular_constraints.p','rb'))
-scores = []
-for tests in cell_tests.values():
-    SA = evaluate(tests,JHH)
-    SA = evaluate(tests,JHH1)
-    SA = evaluate(tests,JHH2)
+    py"""
+    def z_py(evaluate,god):
+        cell_tests = pickle.load(open('multicellular_constraints.p','rb'))
+        tests = cell_tests[list(cell_tests.keys())[0]]
+        SA = evaluate(tests,god)
+        return SA
+    """
 
-    #scores = SA
-    print(SA)
-    scores.append(SA)
 
-"""
-sc = py"scores"
-@show(sc)
-popsize = 200
-nbGenerations = 100
+    function init_function3()
+        py"""
+        grid = ParameterGrid(ranges)
+        genes = []
+        for g in grid:
+            genes.append(list(g.values()))
+        """
+        genes = py"genes"
+        return genes
+    end
 
-#Define the number of division along each objective to generate the reference directions.
-#Alternatively, you can directly pass the reference directions as a Vector{Vector{Float64}} :
-#With two objectives, H = 2 is equivalent to
-#H = [[1., 0.], [0.5, 0.5], [0., 1.]]
+    export init_function
+    function init_function(x)
+        println(x)
+        py"""
+        import random
+        lower_list = [v[0] for k,v in ranges.items()]
+        upper_list = [v[1] for k,v in ranges.items()]
+        genes_out = [random.uniform(lower, upper) for lower, upper in zip(lower_list, upper_list)]
+        """
+        return py"genes_out"#,py"genes_out_dic"
+    end
+    export init_function2
+    function init_function2(x)
+        py"""
+        def one(x):
+            if str('x') in locals():
+                genes_out = x
+                genes_out_dic = OrderedDict({k:genes_out[i] for i,k in enumerate(ranges.keys()) })
+            else:
+                import random
+                lower_list = [v[0] for k,v in ranges.items()]
+                upper_list = [v[1] for k,v in ranges.items()]
+                genes_out = [random.uniform(lower, upper) for lower, upper in zip(lower_list, upper_list)]
+                genes_out_dic = OrderedDict({k:genes_out[i] for i,k in enumerate(ranges.keys()) })
+            return genes_out, genes_out_dic
+        """
+        genes_out, genes_out_dic = py"one"(x)
+        return genes_out, genes_out_dic#py"genes_out",py"genes_out_dic"
+    end
 
-#define how to generate a random genotype :
-#init_function = () -> randperm(N) #e.g : a permutation coding
+    export z
+    function z(x)
+        genes_out,god = init_function2(x)
+        x = genes_out
+        # The slow part can it be done in parallel.
+        contents = py"z_py"(py"evaluate",god)
+        contents = [ convert(Float64,c) for c in contents ]
 
-#define how to evaluate a genotype :
-#z(x) = z1(x), z2(x) ... # Must return a Tuple
+        #y = convert(Tuple64,content)
+        #using NSGAIII
+        #yy = NSGAIII.indiv(y)
+        #print(y)
+        #x.y = y
+        return contents
+    end
 
-#nsga(popsize, nbGenerations, init_function, z, H)
-
-#A constraint violation function can be passed with the keyword fCV
-#It should return 0. if the solution is feasible and a value > 0 otherwise.
-
-#Mutation probability can be changed with the keyword pMut (default is 5%)
-
-ranges = py"ranges"
-H = values(ranges)
-#nsga(popsize, nbGen, init_fun, z, H, fCV = CV, pMut = 0.1)
-"""
-
-popsize = 200
-nbGenerations = 100
-
-##Define the number of division along each objective to generate the reference directions.
-#H = 5
-#Alternatively, you can directly pass the reference directions as a Vector{Vector{Float64}} :
-#With two objectives, H = 2 is equivalent to
-#H = [[1., 0.], [0.5, 0.5], [0., 1.]]
-
-#define how to generate a random genotype :
-function init(H)
-    return genes
 end
-
-MU = 2
-NGEN = 2
-
-function zzz(in_genes)
-
-    #Convert(fitnesses)
-    jfit = tuple(fitnesses)
-    return jfit#pop
-end
-fitnesses = zzz(genes)
-X = typeof(init())
-fCV=(x)->0
-seed = 1.0
-P = [indiv(genes[1], zzz, fCV) for _=1:MU-length(seed)]
-append!(P, indiv.(convert.(X, seed),z, fCV))
-fast_non_dominated_sort!(P)
-associate_references!(P, references)
-Q = similar(P)
-=#
-try
-   nsga(MU, NGEN, init, zzz, fplot = plot_pop)
-catch
-   P = [indiv(genes[1], zzz, fCV) for _=1:MU-length(seed)]
-   append!(P, indiv.(convert.(X, seed),z, fCV))
-   fast_non_dominated_sort!(P)
-   associate_references!(P, references)
-   Q = similar(P)
-end
-"""
