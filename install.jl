bbd029d3fa9d49f94f122eef5ef7c2ccd7f33425
Russell Jarvis <rjjarvis@asu.edu>
2020-04-19 20:21:36 +1000

    Update install.jl

--- install.jl
+++ install.jl
@@ -45,7 +45,7 @@ sys.path.append(os.getcwd())
 py"""
 from neuronunit import tests
 from neuronunit.tests import fi
-from simple_with_injection import SimpleModel
+from examples.simple_with_injection import SimpleModel
 from neo import AnalogSignal
 import quantities as pq
 try:
@@ -57,45 +57,3 @@ except:
 
 
 """
-#=
-simple = py"SimpleModel()"
-
-py"""
-import pickle
-from neuronunit.tests.fi import RheobaseTestP
-
-cell_tests = pickle.load(open('multicellular_constraints.p','rb'))
-for test in cell_tests.values():
-    if "Rheobase test" in test.keys():
-        temp_test = {k:v for k,v in test.items()}
-        break
-rt = temp_test["Rheobase test"]
-
-rtest = RheobaseTestP(observation=rt.observation)
-#                        name='RheobaseTest')
-JHH = {
-'Vr': -68.9346,
-'Cm': 0.0002,
-'gl': 1.0 * 1e-5,
-'El': -65.0,
-'EK': -90.0,
-'ENa': 50.0,
-'gNa': 0.02,
-'gK': 0.006,
-'Vt': -63.0
-}
-JHH1 = {k:(v-0.01*v) for k,v in JHH.items()}
-JHH2 = {k:(v+0.01*v) for k,v in JHH.items()}
-ranges = {k:[v-0.01*v,v+0.01*v] for k,v in JHH.items()}
-
-
-"""
-#
-using OrderedCollections
-ranges = OrderedDict(py"ranges")
-H1=[values(ranges)]
-
-current_params = py"rt.params"
-print(current_params)
-simple.attrs = py"JHH"
-=#
