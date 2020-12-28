01658d6ed29cb436141f822b83e9525d6b7c5b23
Russell <russelljarvis@protonmail.com>
2020-12-28 15:19:41 +1100

    remove debug comments

--- src/main.jl
+++ src/main.jl
@@ -22,20 +22,6 @@ function sim!(P, C; dt = 0.1ms, duration = 10ms)
 	end
 end
 
-	#println(size(spike_raster))
-	#println(P.sized)
-
-    #if hasproperty(P, :spike_raster)
-		#P.spike_raster::Vector{SNNInt} = zeros(trunc(Int, size))
-		#spike_raster::VFT = fill(0, N)
-
-		#P.spike_raster::Vector{Int32} = zeros(trunc(Int, size))
-		#println(P.spike_raster)
-    #end
-    #for t = 0ms:dt:(duration - dt)
-    #    sim!(P, C, dt)
-    #end
-
 function train!(P, C, dt, t = 0)
     for p in P
         integrate!(p, p.param, Float32(dt))
