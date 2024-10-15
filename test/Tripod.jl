486ed0e6422826950161f8a63730a5f44b26e48d
Alessio <alessio.quaresima@mpi.nl>
2024-10-15 02:56:35 +0200

    Tripod test synapses

---
+++ test/Tripod.jl
@@ -0,0 +1,28 @@
+using DrWatson
+using Plots
+using Revise
+using SpikingNeuralNetworks
+using SNNUtils
+SNN.@load_units;
+
+##
+# Define neurons and synapses in the network
+N = 100
+E = SNN.Tripod(
+    (300um), (150um),
+    N = N,
+    soma_syn = Synapse(DuarteGluSoma, MilesGabaSoma),
+    dend_syn = Synapse(EyalGluDend, MilesGabaDend),
+    NMDA = SNN.EyalNMDA,
+    param = SNN.AdExSoma(b = 0.0f0, Vr = -50),
+)
+
+E_to_E = SNN.CompartmentSynapse(
+    E,
+    E,
+    :d1,
+    :exc,
+    p = 0.2,
+    σ = 30,
+    param = SNN.vSTDPParameter(),
+)
