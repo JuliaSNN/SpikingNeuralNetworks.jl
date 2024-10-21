558ea670968c428c313dc99f9282b8c1040660b8
Alessio <alessio.quaresima@mpi.nl>
2024-10-21 15:30:28 +0200

    PoissonStimulus has doc

--- src/stimulus/poisson_stim.jl
+++ src/stimulus/poisson_stim.jl
@@ -22,8 +22,29 @@
 end
 
 
-function PoissonStimulus(post::T, sym::Symbol, r::Union{Function, Float32}, cells=[]; N_pre::Int=50, p_post::R=0.05f0, σ::R=1.f0, param=PoissonParameter()) where {T <: AbstractPopulation, R <: Number}
+"""
+    PoissonStimulus(post::T, sym::Symbol, r::Union{Function, Float32}, cells=[]; N_pre::Int=50, p_post::R=0.05f0, σ::R=1.f0, param=PoissonParameter()) where {T <: AbstractPopulation, R <: Number}
 
+Constructs a PoissonStimulus object for a spiking neural network.
+
+# Arguments
+- `post::T`: The target population for the stimulus.
+- `sym::Symbol`: The symbol representing the synaptic conductance or current.
+- `r::Union{Function, Float32}`: The firing rate of the stimulus. Can be a constant value or a function of time.
+- `cells=[]`: The indices of the cells in the target population that receive the stimulus. If empty, cells are randomly selected based on the probability `p_post`.
+- `N_pre::Int=50`: The number of presynaptic cells.
+- `p_post::R=0.05f0`: The probability of connection between presynaptic and postsynaptic cells.
+- `σ::R=1.f0`: The scaling factor for the synaptic weights.
+- `param=PoissonParameter()`: The parameters for the Poisson distribution.
+
+# Returns
+A `PoissonStimulus` object.
+"""
+function PoissonStimulus(post::T, sym::Symbol, r::Union{Function}; cells=[], N_pre::Int=50, p_post::R=0.05f0, σ::R=1.f0, param=PoissonParameter()) where {T <: AbstractPopulation, R <: Number}
+
+    if cells == :ALL
+        cells = 1:post.N
+    end 
     if isempty(cells)
         for i in  1:post.N
             if rand() < p_post
@@ -31,12 +52,12 @@ function PoissonStimulus(post::T, sym::Symbol, r::Union{Function, Float32}, cell
             end
         end
     end
-    w = (N_pre*p_post) * ceil.(sprand(length(cells), N_pre, p_post)) # Construct a random sparse matrix with dimensions post.N x pre.N and density p
+    w = ceil.(sprand(length(cells), N_pre, 0.2)) # Construct a random sparse matrix with dimensions post.N x pre.N and density p
     w = SNN.dropzeros(w .* σ ./sum(w, dims=2))
 
     rowptr, colptr, I, J, index, W = dsparse(w)
     if typeof(post) <: AbstractDendriteIF
-        g_d = view(getfield(post, sym), :, [1,2])
+        g_d = view(getfield(post, sym), :, [1])
         g = []
     else
         g = getfield(post, sym)
@@ -66,27 +87,26 @@ Poisson
 
 function stimulate!(p::PoissonStimulus, param::PoissonParameter, time::Time, dt::Float32)
     @unpack N, randcache, fire, rate, cells, colptr, W, I, g, g_d = p
+    myrate = rate(get_time(time))
     rand!(randcache)
+    @inbounds @simd for j = 1:N
+        if randcache[j] < myrate * dt
+            fire[j] = true
+        end
+    end
     if isempty(g) 
-        myrate::Float32 = rate(time)
-        @inbounds for j = 1:N
-            if randcache[j] < myrate * dt
-                fire[j] = true
-                @inbounds for i ∈ eachindex(cells) # loop on postsynaptic cells
-                    @inbounds @fastmath @simd for s ∈ colptr[j]:(colptr[j+1]-1)
-                        g_d[cells[I[s]]] += W[s]
-                    end
+        for j = 1:N # loop on presynaptic cells
+            if fire[j] # presynaptic fire
+                @fastmath @simd for s ∈ colptr[j]:(colptr[j+1]-1)
+                    g_d[cells[I[s]]] += W[s]
                 end
             end
         end
     else
-        @inbounds for j = 1:N
-            if randcache[j] < myrate * dt
-                fire[j] = true
-                @inbounds for i ∈ eachindex(cells) # loop on postsynaptic cells
-                    @inbounds @fastmath @simd for s ∈ colptr[j]:(colptr[j+1]-1)
-                        g[cells[I[s]]] += W[s]
-                    end
+        for j = 1:N
+            if fire[j] # presynaptic fire
+                @fastmath @simd for s ∈ colptr[j]:(colptr[j+1]-1)
+                    g[cells[I[s]]] += W[s]
                 end
             end
         end
