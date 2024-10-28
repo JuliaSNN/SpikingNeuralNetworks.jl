43cadcbca02b68ae9348a12f0e628703653948d7
galborsz <galborsz@gmail.com>
2024-10-28 14:59:32 +0100

    updated structs to match main

--- src/structs.jl
+++ src/structs.jl
@@ -2,14 +2,13 @@ abstract type AbstractParameter end
 abstract type AbstractConnectionParameter <: AbstractParameter end
 abstract type AbstractPopulationParameter <: AbstractParameter end
 abstract type AbstractStimulusParameter <: AbstractParameter end
-abstract type AbstractNeuronParameter <: AbstractParameter end
-abstract type AbstractSynapseParameter <: AbstractParameter end
 abstract type AbstractConnection end
 abstract type AbstractPopulation end
 abstract type AbstractStimulus end
 
 abstract type AbstractSparseSynapse <: AbstractConnection end
 abstract type AbstractNormalization <: AbstractConnection end
+abstract type PlasticityVariables end
 
 Spiketimes = Vector{Vector{Float32}}
 
@@ -20,3 +19,26 @@ end
 
 
 export Spiketimes
+
+# abstract type AbstractParameter end
+# abstract type AbstractConnectionParameter <: AbstractParameter end
+# abstract type AbstractPopulationParameter <: AbstractParameter end
+# abstract type AbstractStimulusParameter <: AbstractParameter end
+# abstract type AbstractNeuronParameter <: AbstractParameter end
+# abstract type AbstractSynapseParameter <: AbstractParameter end
+# abstract type AbstractConnection end
+# abstract type AbstractPopulation end
+# abstract type AbstractStimulus end
+
+# abstract type AbstractSparseSynapse <: AbstractConnection end
+# abstract type AbstractNormalization <: AbstractConnection end
+
+# Spiketimes = Vector{Vector{Float32}}
+
+# @snn_kw struct EmptyParam
+#     type::Symbol = :empty
+# end
+
+
+
+# export Spiketimes
