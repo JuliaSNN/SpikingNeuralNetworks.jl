18f05c9adfac23e82f0f31ee3e083cdf62034db2
Alessio Quaresima <alessio.quaresima@pasteur.fr>
2025-04-10 15:29:22 +0200

    synapse parameters soma

--- src/neuron/synapse_parameters.jl
+++ src/neuron/synapse_parameters.jl
@@ -24,6 +24,24 @@ MilesGabaDend = GABAergic(
 TripodSomaSynapse = Synapse(DuarteGluSoma, MilesGabaSoma)
 TripodDendSynapse = Synapse(EyalGluDend, MilesGabaDend)
 
+
+## Soma parameters
+
+Mg_mM = 1.0mM
+nmda_b = 3.57   # voltage dependence of nmda channels
+nmda_k = -0.062     # Eyal 2018
+SomaNMDA = NMDAVoltageDependency(mg = Mg_mM/mM, b = nmda_b, k = nmda_k)
+SomaGlu = Glutamatergic(
+    Receptor(E_rev = 0.0, τr = 1ms, τd = 6.0ms, g0 = 0.7),
+    ReceptorVoltage(E_rev = 0.0, τr = 1ms, τd = 100.0, g0 = 0.15, nmda = 1.0f0),
+)
+SomaGABA = GABAergic(
+    Receptor(E_rev = -70.0, τr = 0.5, τd = 10.0, g0 = 2.),
+    Receptor(E_rev = -90.0, τr = 30, τd = 400.0, g0 = 0.006), # τd = 100.0
+)
+SomaNMDA = NMDAVoltageDependency()
+SomaSynapse = Synapse(SomaGlu, SomaGABA)
+
 ## CAN_AHP parameters
 
 Glu_CANAHP = Glutamatergic(
@@ -41,3 +59,5 @@ nmda_b = 3.57
 nmda_k = -0.063 
 NMDA_CANAHP = NMDAVoltageDependency(mg = Mg_mM/mM, b = nmda_b, k = nmda_k)
 
+
+export SomaNMDA, SomaSynapse, TripodSomaSynapse, TripodDendSynapse, EyalNMDA, NMDA_CANAHP, Synapse_CANAHP
