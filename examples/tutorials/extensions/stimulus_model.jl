0cd90a67c36ec0a17183bc94a23d4d1dfedb7597
Alessio <alessio.quaresima@mpi.nl>
2025-08-14 11:40:19 +0200

    added stimulus model example

--- examples/tutorials/extensions/stimulus_model.jl
+++ examples/tutorials/extensions/stimulus_model.jl
@@ -17,8 +17,9 @@ using ProtoStructs
     # @proto struct PoissonRefractoryParameter{R = Float32} 
         ΔT::Float32 = 2f0ms  # Absolute refractory period
         N::Int = 100  # Number of neurons
+        rate::Float32 = 10Hz
         last_spike::Vector{Float32} = zeros(Float32, N)  # Last spike time for each neuron
-        rate::Vector{Float32} = ones(Float32, N)  # Firing rate for each neuron
+        rates::Vector{Float32} = fill(rate, N)  # Firing rate for each neuron
         p::Float32 = 0.1f0  # Fraction of neurons receiving the stimulus
         μ::Float32 = 1f0  # Mean of the weight distribution
         σ::Float32 = 0f0  # Standard deviation of the weight distribution
@@ -35,11 +36,11 @@ using ProtoStructs
         dt::Float32,
     )
         @unpack N, randcache, fire, neurons, colptr, W, I, g = p
-        @unpack rate, ΔT, last_spike = param
+        @unpack rates, ΔT, last_spike = param
         current_time = get_time(time)
         rand!(randcache)
         @inbounds @simd for j = 1:N
-            if (current_time - last_spike[j]) > ΔT && randcache[j] < rate[j] * dt
+            if (current_time - last_spike[j]) > ΔT && randcache[j] < rates[j] * dt
                 fire[j] = true
                 last_spike[j] = current_time
                 @fastmath @simd for s ∈ colptr[j]:(colptr[j+1]-1)
@@ -57,15 +58,15 @@ import SpikingNeuralNetworks: PoissonLayer, PoissonRefractoryParameter, compose,
 # validate_population_model(SNN.Neuron()) # This is only available in SNNModels v1.5.5
 
 neuron_param = SNN.IdentityParam()
-neuron = SNN.Identity(; param = param, N = 1, name = "Identity Neuron")
+neuron = SNN.Identity(; param = neuron_param, N = 1, name = "Identity Neuron")
 
 # Create a withe noise input current 
-stim_param = PoissonRefractoryParameter(N=1, ΔT = 10ms, p=1)
-stim = PoissonLayer(neuron, :g; param=current_param)
+stim_param = PoissonRefractoryParameter(N=1, ΔT = 20ms, p=1)
+stim = PoissonLayer(neuron, :g; param=stim_param)
 
 monitor!(neuron, [:g, :fire], sr = 2kHz)
 model = compose(; neuron, stim)
-sim!(; model, duration = 1000ms, pbar=true)
+sim!(; model, duration = 100000ms, pbar=true)
 
 vecplot(
     neuron,
@@ -73,6 +74,10 @@ vecplot(
     neurons=1,
     add_spikes = true,
     ylabel = "Membrane potential (mV)",
+    xlims = (0, 1000ms),
     # ylims = (-80, 10),
     c = :black,
 )
+
+st = SNN.spiketimes(neuron)[1]
+diff(st) |> x-> SNNPlots.histogram(x, bins=100)
