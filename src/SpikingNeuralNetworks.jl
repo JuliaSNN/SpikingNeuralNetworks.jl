46cef48b2f434c56219e472111b72d746011d427
Alessio <alessio.quaresima@mpi.nl>
2025-09-11 10:52:51 +0200

    added modules to doc

--- src/SpikingNeuralNetworks.jl
+++ src/SpikingNeuralNetworks.jl
@@ -10,4 +10,14 @@ export SNNModels, SNNPlots, SNNUtils
 SNN = SpikingNeuralNetworks
 export SNN
 
+"""
+porcodio
+"""
+f
+
+function f()
+    @info "SpikingNeuralNetworks.jl is loaded. Use SNNModels, SNNPlots, and SNNUtils for your spiking neural network needs."
+    return nothing
+end
+
 end
